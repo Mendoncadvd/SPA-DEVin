fecdbe86a679eb06fee84cd3f67f8d4fb15bf9a6
David Mendonça <davidmed66@gmail.com>
2021-01-16 19:27:10 -0300

    Correção das celulas da tabela

--- src/pages/listagem/Listagem.jsx
+++ src/pages/listagem/Listagem.jsx
@@ -9,7 +9,12 @@ const Listagem = () => {
                 <TableBody>
                     {linhas.map((linha) => (
                         <TableRow key={linha.id}>
-                            <TableCell align="center">{linha.numero}</TableCell>
+                            <TableCell>
+                                {/* Lembrar de tirar a linha da celula */}
+                                <TableHead align="center">Teste</TableHead>
+                                <TableCell align="center">{linha.numero}</TableCell>
+                            </TableCell>
+                            
                             <TableCell align="center">{linha.assunto}</TableCell>
                             <TableCell align="center">{linha.interessado}</TableCell>
                             <TableCell align="center">{linha.descricao}</TableCell>
