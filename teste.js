76973aae96368c8caaf336f132ca42630b50cef0
David Mendonça <davidmed66@gmail.com>
2021-01-24 21:15:43 -0300

    deleção de arquivo

--- teste.js
+++
@@ -1,9 +0,0 @@
-const linhas =[{
-    id: 1, 
-    numero: "000", 
-    assunto: "qualquer", 
-    interessado: "fulano", 
-    descricao: "blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla"
-}]
-const linha = linhas.map(element => element.numero)
-console.log(linha)
